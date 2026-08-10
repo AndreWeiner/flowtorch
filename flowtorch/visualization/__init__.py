6e0d7171995935c06dfc7c4b94a45a58b150691e
Andre Weiner <andre.weiner@tu-dresden.de>
2026-08-10 11:16:08 +0200

    Add plotting functions for 2D image-like SPOD modes.

--- flowtorch/visualization/__init__.py
+++ flowtorch/visualization/__init__.py
@@ -10,6 +10,8 @@ _EXPORTS: Dict[str, Tuple[str, str]] = {
     ),
     "animate_scalar_field": ("animations", "animate_scalar_field"),
     "line_integral_convolution": ("lic", "line_integral_convolution"),
+    "plot_spod_mode_2d": ("modes", "plot_spod_mode_2d"),
+    "plot_spod_modes_2d": ("modes", "plot_spod_modes_2d"),
     "plot_spod_spectra": ("spectra", "plot_spod_spectra"),
     "plot_spod_spectrum": ("spectra", "plot_spod_spectrum"),
 }
